32d9796704344f93bb0e228959c0f5f43fc61402
Thanmai Nallani <Nallanithanmai160@gmail.com>
2026-03-29 13:42:59 -0500

    Refactor health_app.py to include XGBoost

--- health_app.py
+++ health_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import pickle        
+import xgboost as xgb
 
 # 1. PAGE SETUP
 st.set_page_config(page_title="MyHealth Personal Dashboard", page_icon="👤", layout="wide")
